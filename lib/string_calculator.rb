bb3906b6a4117c0e5f3438d16b97586d4026e8eb
jmks <jason.schweier@gmail.com>
2015-02-28 16:04:20 -0500

    8 implementation

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,16 @@
 module StringCalculator
 
   def self.add(string)
-    string.scan(/\d+/).map(&:to_i).inject(0, :+)
+    numbers = get_numbers(string)
+
+    if numbers.any? { |i| i < 0 }
+      raise "Negative number error"
+    else
+      numbers.inject(0, :+)
+    end
+  end
+
+  def self.get_numbers(string)
+    string.scan(/-?\d+/).map(&:to_i)
   end
 end
