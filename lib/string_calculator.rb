683aa96a045f7f613d72fe4dda962e2d38a66a64
jmks <jason.schweier@gmail.com>
2015-02-28 15:22:03 -0500

    4 min implementation

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,8 @@
 module StringCalculator
 
   def self.add(string)
-    if string.include? ","
-      string[0].to_i + string[2].to_i
+    if string =~ /(\d+),(\d+)/
+      $1.to_i + $2.to_i
     elsif string == "5"
       5
     else
