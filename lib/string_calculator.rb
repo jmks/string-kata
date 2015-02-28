7f6f2c1213fe732877fd5a0bf520741dfe684966
jmks <jason.schweier@gmail.com>
2015-02-28 15:30:57 -0500

    5 min implementation

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,12 +1,6 @@
 module StringCalculator
 
   def self.add(string)
-    if string =~ /(\d+),(\d+)/
-      $1.to_i + $2.to_i
-    elsif string == "5"
-      5
-    else
-      0
-    end
+    string.split(',').map(&:to_i).inject(0, :+)
   end
 end
